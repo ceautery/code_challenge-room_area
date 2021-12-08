7aa141eb04c8e6799387602aed5eb0c1cb61940f
Curtis Autery <curtis@rvshare.com>
2021-12-08 10:07:00 -0500

    CoordMaker inits with vectors, returns AofA

--- coord_maker.rb
+++ coord_maker.rb
@@ -1,3 +1,7 @@
 class CoordMaker
+  attr_reader :coords
 
+  def initialize(vectors)
+    @coords = [[0, 0], [0, 4]]
+  end
 end
--- coord_maker_spec.rb
+++ coord_maker_spec.rb
@@ -2,4 +2,13 @@ require 'rspec'
 require_relative 'coord_maker'
 
 RSpec.describe CoordMaker do
+  subject { CoordMaker.new(vectors) }
+
+  context 'with a single vector' do
+    let(:vectors) { [['N', 4]] }
+
+    it 'returns (0,0) and (0,4)' do
+      expect(subject.coords).to contain_exactly([0, 0], [0, 4])
+    end
+  end
 end
