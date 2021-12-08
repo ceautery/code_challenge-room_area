97e9e1e2d81b05e46619da32544dac68ef0b1579
Curtis Autery <curtis@rvshare.com>
2021-12-08 11:15:46 -0500

    Room calculates area using the shoelace formula

--- room.rb
+++ room.rb
@@ -2,6 +2,15 @@ class Room
   attr_reader :area
 
   def initialize(coords)
-    @area = 15
+    working = 0
+
+    coords.each_cons(2) do |p1, p2|
+      a, b = p1
+      c, d = p2
+
+      working += (a * d - b * c)
+    end
+
+    @area = working.abs / 2
   end
 end
